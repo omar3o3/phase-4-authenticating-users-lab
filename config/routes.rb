9335683ff85c3807f83c49d17eab4e02bb447744
omar3o3 <otantawy53@gmail.com>
2022-07-11 17:02:54 -0400

    commit tree 3bc5576297379fadbb7d7dcc9c567a9d7d08a0a7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+
+  get '/me', to: 'users#show'
 end
